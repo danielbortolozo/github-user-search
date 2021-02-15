c7f9bab193dfb3321d8176feefb206ea815b772c
Daniel Rocca Bortolozo <danielbortolozo@hotmail.com>
2021-02-15 11:51:48 -0300

    Adding loaders, data com dayjs.

---
+++ src/core/components/types/PerfilUsuario.ts
@@ -0,0 +1,13 @@
+export type PerfilUsuario = {
+    
+    avatar_url: string;      //- imagem
+    followers: number;       //-seguidores 
+    following: number;       //-seguindo
+    public_repos: number;    //-repositorios publicos
+    company: string;         //-empresa
+    location: string;        //-localizacao
+    blog: string;            //-website/blog
+    created_at:string;      //-membro desde.
+     
+
+}
--- src/pages/Search/index.tsx
+++ src/pages/Search/index.tsx
@@ -1,7 +1,11 @@
 import React, { useState } from 'react';
 import { makeRequest } from 'core/components/utils/request';
 import './styles.scss';
-
+import { PerfilUsuario } from 'core/components/types/PerfilUsuario';
+import InfoLaders from './components/Loaders/InfoLoader';
+import ImagemLoader from './components/Loaders/ImagemLoader';
+import 'dayjs/locale/pt-br'
+import dayjs from 'dayjs';
 
 type FormState = {
    usuario: string;
@@ -12,9 +16,13 @@ type FormEvent = React.ChangeEvent<HTMLInputElement | HTMLSelectElement | HTMLTe
 const Search = () => {
 
    const [formData, setformData] = useState<FormState>({
-      usuario: '',      
+      usuario: '', 
+
   });
-  
+
+  const [perfilUsuario, setPerfilUsuario] = useState<PerfilUsuario>();
+  const [isLoading, setIsLoading] = useState(false);
+  const [isPainelPerfil, setIsPainelPerfil] = useState(false);
 
   const handleOnChange = (event: FormEvent) => {
      const name = event.target.name;
@@ -24,15 +32,45 @@ const Search = () => {
   }
   
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) =>{
-      event.preventDefault();     
-      
-    if (formData.usuario)
-       makeRequest({ url: '/users/'+formData.usuario, method: 'GET'})
-    else{
-       alert('Nenhum usuário digitado');
-       return;
-    }    
+     event.preventDefault();     
+     if (formData.usuario){ 
+        setIsLoading(true);        
+        setIsPainelPerfil(true);  
+        makeRequest({ url: '/users/'+formData.usuario, method: 'GET'})
+        .then(response => {          
+         setPerfilUsuario(response.data); 
+
+         
+        
+        console.log('Company = ', perfilUsuario?.company)
+       }).finally(() => {           
+            setIsLoading(false);
+          })
+        .catch(error => {
+           console.log('Error catch :', error.statusText);
+           setIsPainelPerfil(false);
+           alert('Usuário não encontrado! Tente outro usuário.')
+        })  
+         
+         
+     }
+      else{
+          alert('Nenhum usuário digitado');
+          setIsPainelPerfil(false);
+          return;
+      }    
   }
+
+
+  //variaveis do perfil do usuário
+  var repositorio_publico: string = 'Repositório público: '+ perfilUsuario?.public_repos ;
+  var seguidores: string = 'Seguidores: '+perfilUsuario?.followers;
+  var seguindo: string = 'Seguindo: '+perfilUsuario?.following; 
+  var company: string = 'Comapany : '+perfilUsuario?.company;
+  var website: string = 'WebSite/Blog : '+perfilUsuario?.blog;
+  var localidade: string = 'Localidade : '+perfilUsuario?.location;
+  var desde: string = 'Membro desde: '+dayjs(perfilUsuario?.created_at).format('DD/MM/YYYY');
+
 
    return (
 
@@ -58,8 +96,47 @@ const Search = () => {
                     Encontrar
               </button>     
           </div>
-        </form>                   
+        </form>   
+
+
+        {!isPainelPerfil ? <> </> : (
+          
+          <div className="painel-perfil">
+
+          {isLoading ? <>
+                         <div className="info-loaders"><InfoLaders /></div> 
+                         <div className="img-loaders"><ImagemLoader /></div>
+                       </>: (
+               <>                 
+                  
+                     <div >
+                       <img src={perfilUsuario?.avatar_url} alt="avatar" className="img"/> 
+                       <button className="btn-perfil"> Ver perfil</button>
+                     </div>
+                                               
+                 
+                 <div >
+                 <input  value={repositorio_publico}  className="input-repositori"/> 
+                 <input value={seguidores} className="input-seguidores" />  
+                 <input value={seguindo} className="input-seguindo" />
+                 </div>
+                 <div className="painel-interno">
+                 <label className="painel-interno-label">Informações </label>
+
+                 <input className="input-company" value={company} />
+                 <input className="input-website" value={website} />
+                 <input className="input-localidade" value={localidade} />
+                 <input className="input-desde" value={desde} />
+                 </div>    
+
+                 
+              </>
+           )}
+          </div>  
+        )}
+       
       </div>
+      
    );
 
 
